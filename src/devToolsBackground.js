44fd6ff7b38ac941ddf43953615e18080e6df260
Frank Schumacher <frank.schumacher.privat@googlemail.com>
2016-07-13 23:12:11 +0200

    cleaify

--- src/devToolsBackground.js
+++ src/devToolsBackground.js
@@ -1,7 +1,10 @@
 //console.log(WebInspector.scriptSnippetModel);
 var sourcesListener = function(whatEver) {
+  // THis should be called but isn't
   console.log("[snippets]", whatEver);
 };
 
+// register a listener for the sources panel.
 chrome.devtools.panels.sources.onSelectionChanged.addListener(sourcesListener);
+
 console.log("[snippets] added listener");
